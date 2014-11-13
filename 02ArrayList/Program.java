04b7763ed61852e69f9c162770707b182c1b1502
tiffanym <huntergirl2015@gmail.com>
2014-11-12 19:06:25 -0500

    So confused right now. This is what happens when I miss a day of class. Sort of guessed at what ArrayList was, online resources made me more confused; just gave up T^T

---
+++ 02ArrayList/Program.java
@@ -0,0 +1,27 @@
+package program;
+import java.util.ArrayList;
+
+public class Program{
+    public void collapseDuplicates(ArrayList<Integer> L){
+	for (int i=0;i<L.length-1;i++){
+	    if (L[i]==L[i+1]){
+		L[i]=null;
+		L[i+1]=null;
+		i=i+1;
+	    }
+	}
+    }
+
+    //  0,1,2,3,4
+    //  5,6,1,5,5
+    public static void main(String[]args){
+	ArrayList<Integer> elements=new ArrayLIst<>();
+	elements.add(2);
+	elements.add(3);
+	elements.add(4);
+	elements.add(4);
+	elements.add(3523);
+	
+	collaseDuplicates(elements);
+    }
+}
