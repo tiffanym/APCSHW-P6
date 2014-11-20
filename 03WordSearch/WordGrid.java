f1d9f14de60819f758568ccd1292ad74a5b63b88
tiffanym <huntergirl2015@gmail.com>
2014-11-20 12:28:12 -0500

    Everything

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -16,7 +16,7 @@ public class WordGrid{
     private void clear(){
 	for (int i=0;i<data.length;i++){
 	    for (int x=0;x<data[i].length;x++){
-		data[i][x]=' ';
+		data[i][x]='_';
 	    }
 	}
     }
@@ -47,15 +47,45 @@ public class WordGrid{
      *or there are overlapping letters that do not match, then false is returned.
      */
     public boolean addWordHorizontal(String word,int row, int col){
-	int width=data[0].size(); //total number of columns
-	//data.length=number of rows
-	if (word.length()+col<width){
+	int width=data[0].length;
+	if (word.length()+col>width){
 	    return false;
 	}	
-	for (int i=col;i<word.length()+col;i++){
-	    data[row][i]=word.charAt(i-col);
+	else{
+	    for (int i=col;i<word.length()+col;i++){
+		if
+		data[row][i]=word.charAt(i-col);
+	    }
+	    return true;
 	}
-	return true;
+    }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top to bottom, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordVertical(String word,int row, int col){
+	
+    }
+
+    /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from top left to bottom right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordDiagonal(String word,int row, int col){
+
     }
 
     //vertical + diagonal should be implemented as well.
