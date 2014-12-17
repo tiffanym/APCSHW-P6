9c54980870bcee6b7e47011047ee130c1b774f94
tiffanym <huntergirl2015@gmail.com>
2014-12-16 20:43:43 -0500

    bubbleSort worked and fixed some minor issues with insertionSort and selectionSort methods

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -1,26 +1,28 @@
+import java.util.*;
+
 public class Sorts{
-    int[] data;
-    /*
+
     public static void insertionSort(int[] data){
 	if (data.length>1){
 	    for (int i=1;i<data.length;i++){
 		int element=data[i];
-		if (element.compareTo(data[i-1])<0){
+		if (element-data[i-1]<0){
 		    int x;
 		    if (i>1){
 			x=i-2;
-			while(data[x].compareTo(element)>0 && x>0){
+			data[i]=data[i-1];
+			while(x>-1 && data[x]-element>0){
 			    data[x+1]=data[x];
 			    x--;
 			}
-			if (x>0){
-			    x+=1;
+			
+			if (x>=0){
+			    data[x+1]=element;
 			}
-			for (int a=i;a>x;a--){
-			    data[a]=data[a-1];
+			else if (x<0){
+			    data[0]=element;
 			}
-			data[x]=element;
-
+			
 		    }
 		    else{
 			data[i]=data[i-1];
@@ -30,42 +32,38 @@ public class Sorts{
 	    }
 	}
     }
-
+    
     public static void selectionSort(int[] data){
 	for (int i=0;i<data.length;i++){
-	    int min=findMinIndex(i,data.length-1);
-	    int sAtMin=data[min];
-	    for (int x=min;x>i;x--){
+	    int minIndex=i;
+	    for (int a=i;a<data.length;a++){
+		if (data[a]-data[minIndex]<0){
+		    minIndex=a;
+		}
+	    }	   
+	    int sAtMin=data[minIndex];
+	    for (int x=minIndex;x>i;x--){
 		data[x]=data[x-1];
 	    }
 	    data[i]=sAtMin;
 	}
     }
-    */
 
-    public boolean checkNoSwaps(int[] data,int index){
-	boolean swapsLeft=true;
-	for (int i=index;i>0;i--){
-	    if (data[i+1]<data[i]){
-		swapsLeft=false;
-		break;
+    public static void bubbleSort(int[] data){	
+	for (int end=data.length-1;end>0;end--){
+	    boolean swapsLeft=false;
+	    for (int x=end-1;x>0;x--){
+		if (data[x+1]<data[x]){
+		    swapsLeft=true;
+		    break;
+		}
 	    }
-	}
-	return swapsLeft;
-    }
-
-    //moves this way <------from end
-    public static void bubbleSort(int[] data){
-	int end=data.length-1;
-	//if false; then stop
-	boolean swapsLeft=true;
-	for (int i=end;i>-1;i--){
-	    if (checkNoSwaps(data,i)){
-		for (int x=0;x<end;x++){
-		    if (data[x]>data[x+1]){
-			int temp=data[x];
-			data[x]=data[x+1];
-			data[x+1]=temp;
+	    if (swapsLeft){
+		for (int i=0;i<end;i++){
+		    if (data[i]>data[i+1]){
+			int temp=data[i];
+			data[i]=data[i+1];
+			data[i+1]=temp;
 		    }
 		}
 	    }
@@ -77,8 +75,11 @@ public class Sorts{
 
     public static void main(String[]args){
 	int[] data = new int[]{5,10,4,12,-5,1,0};
+	System.out.println(Arrays.toString(data));
 	bubbleSort(data);
-	System.out.println(data.toString());
+	//selectionSort(data);
+	//insertionSort(data);
+	System.out.println(Arrays.toString(data));
     }
 
 }
